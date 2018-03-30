267ffdff2bb9e86235a7cf943670439ce20f6324
G M <gmarat19@gmail.com>
2018-03-30 00:20:49 -0500

    add check_grad which has working gradient computation, and add sgd which implements SGD

--- code_stopt/check_grad.py
+++ code_stopt/check_grad.py
@@ -1,17 +1,16 @@
 import numpy as np, read_data, prob_grad
 from scipy.optimize import check_grad
 
+l = 1e-6
 data = read_data.read_train_sgd()
 
 def func(params, *args):
 #computes function value for a single example
-#note that parameters are not directly accessed
-#instead they are manipulated through a view
 
 	W, T = params[:26*129].reshape((26, 129)),\
 		params[26*129:].reshape((26, 26))
-	x, y = args[2]
-	l = args[3]
+	x, y = args[0]
+	l = args[1]
 
 	log_p = prob_grad.compute_log_p(x, y, W, T)
 	
@@ -24,15 +23,14 @@ log_grad = np.zeros(26*129+26*26)
 l_gw, l_gt = log_grad[:26*129].reshape((26, 129)),\
 	log_grad[26*129:].reshape((26, 26))
 
+
 def func_prime(params, *args):
 #computes the derivative of a single example
-#note that parameters are not directly accessed
-#instead they are manipulated through a view
 
 	W, T = params[:26*129].reshape((26, 129)),\
 		params[26*129:].reshape((26, 26))
-	x, y = args[2]
-	l = args[3]
+	x, y = args[0]
+	l = args[1]
 
 	#compute first part of objective
 	np.multiply(prob_grad.log_p_wgrad(W, x, y, T), -1, out=l_gw)
@@ -43,9 +41,6 @@ def func_prime(params, *args):
 
 	return log_grad
 
-l = 0.5
 params = np.random.rand((26*129+26*26))
-W, T = params[:26*129].reshape((26, 129)),\
-	params[26*129:].reshape((26, 26))
 
-print(check_grad(func, func_prime, params, W, T, data[7], l))
+print(check_grad(func, func_prime, params, data[7], l))
--- code_stopt/sgd.py
+++ code_stopt/sgd.py
@@ -2,56 +2,55 @@ import numpy as np, read_data, prob_grad, random
 
 norm = np.zeros(26)
 
-#function computation
 def func(params, *args):
-#objective function specified in the handout
-	W, T = params[:26*129].reshape((26, 129)), params[26*129:].reshape((26, 26))
+#computes function value for a single example
+
+	W, T = params[:26*129].reshape((26, 129)),\
+		params[26*129:].reshape((26, 26))
 	data = args[0]
-	lmda = args[1]
+	l = args[1]
 
 	log_p = 0
 	for example in data:
 		log_p += prob_grad.compute_log_p(example[0], example[1], W, T)
-
-	for i in range(26):
-		norm[i] = np.linalg.norm(W[i])
-
-	np.square(norm, out=norm)
 	
-	return -1*log_p/(len(data)) + lmda*0.5*(np.sum(norm)+ np.sum(np.square(T)))
+	return -1*log_p/len(data) + 0.5*l*(\
+		np.sum(np.square(np.linalg.norm(W, axis=1))) +\
+		np.sum(np.square(T)))
 
-log_grad_w = np.zeros(26*129).reshape((26, 129))
-log_grad_t = np.zeros(26*26).reshape((26, 26))
-grad = np.zeros(26*129+26*26)
+
+log_grad = np.zeros(26*129+26*26)
+
+l_gw, l_gt = log_grad[:26*129].reshape((26, 129)),\
+	log_grad[26*129:].reshape((26, 26))
 
 def func_prime(params, *args):
-#derivative of objective function specified in the handout
-	W, T = params[:26*129].reshape((26, 129)), params[26*129:].reshape((26, 26))
+#computes the derivative of a single example
+
+	W, T = params[:26*129].reshape((26, 129)),\
+		params[26*129:].reshape((26, 26))
 	x, y = args[0]
-	lmda = args[1]
+	l = args[1]
 
-	np.multiply(prob_grad.log_p_wgrad(W, x, y, T), -1, out=log_grad_w)
-	np.multiply(prob_grad.log_p_tgrad(T, x, y, W), -1, out=log_grad_t)
+	#compute first part of objective
+	np.multiply(prob_grad.log_p_wgrad(W, x, y, T), -1, out=l_gw)
+	np.multiply(prob_grad.log_p_tgrad(T, x, y, W), -1, out=l_gt)
 
-	#add gradient of norm
-	np.add(log_grad_w, W, out=log_grad_w)
+	#add regularizers
+	np.add(log_grad, np.multiply(l, params), out=log_grad)
 
-	#add normalizing factor
-	np.add(log_grad_t, T, out=log_grad_t)
-
-	np.concatenate([log_grad_w.reshape(26*129),\
-		log_grad_t.reshape(26*26)], out=grad)
 
 #initial guess of 0
 guess = np.zeros((26*129+26*26))
 data = read_data.read_train_sgd()
-learning_rate, l = 1e-5, 1e-2
+learning_rate, l = 1e-3, 1e-2
 
 #run sgd
 for i in range(100):
 	random.shuffle(data)
-	print(func(guess, data, l))
+	if(i % 5 == 0):
+		print(func(guess, data, l))
 	for example in data:
 		func_prime(guess, example, l)
-		np.multiply(learning_rate*-1, grad, out=grad)
-		np.add(guess, grad, out=guess)
+		np.multiply(learning_rate*-1, log_grad, out=log_grad)
+		np.add(guess, log_grad, out=guess)
